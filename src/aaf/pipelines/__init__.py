9c465729a7eda8993e073a40c7639bd0190313c3
atikulmunna <atikul.munna@northsouth.edu>
2026-05-11 21:21:48 +0600

    Export SMD pipelines

--- src/aaf/pipelines/__init__.py
+++ src/aaf/pipelines/__init__.py
@@ -1,1 +1,11 @@
 """Executable experiment pipelines."""
+
+from aaf.pipelines.smd_baseline import SMDBaselineConfig, run_smd_baseline
+from aaf.pipelines.smd_joint import SMDJointConfig, run_smd_joint
+
+__all__ = [
+    "SMDBaselineConfig",
+    "SMDJointConfig",
+    "run_smd_baseline",
+    "run_smd_joint",
+]
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,5 +1,6 @@
 from aaf import __version__
 from aaf.data import SMDMachineSplit
+from aaf.pipelines import SMDBaselineConfig, SMDJointConfig
 
 
 def test_package_version_is_defined() -> None:
@@ -8,3 +9,8 @@ def test_package_version_is_defined() -> None:
 
 def test_data_package_exports_smd_container() -> None:
     assert SMDMachineSplit.__name__ == "SMDMachineSplit"
+
+
+def test_pipeline_package_exports_smd_configs() -> None:
+    assert SMDBaselineConfig.__name__ == "SMDBaselineConfig"
+    assert SMDJointConfig.__name__ == "SMDJointConfig"
