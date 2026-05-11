5e7049498abaefc2577588f1453ad194af62d174
atikulmunna <atikul.munna@northsouth.edu>
2026-05-11 21:21:26 +0600

    Export SMD data helpers

--- src/aaf/data/__init__.py
+++ src/aaf/data/__init__.py
@@ -1,1 +1,23 @@
 """Data generation and dataset utilities."""
+
+from aaf.data.smd import (
+    SMDMachineSplit,
+    SMDPreparedMachine,
+    fit_smd_standardizer,
+    list_smd_machine_ids,
+    load_smd_machine,
+    load_smd_machines,
+    prepare_smd_machine,
+    prepare_smd_windowed_datasets,
+)
+
+__all__ = [
+    "SMDMachineSplit",
+    "SMDPreparedMachine",
+    "fit_smd_standardizer",
+    "list_smd_machine_ids",
+    "load_smd_machine",
+    "load_smd_machines",
+    "prepare_smd_machine",
+    "prepare_smd_windowed_datasets",
+]
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,5 +1,10 @@
 from aaf import __version__
+from aaf.data import SMDMachineSplit
 
 
 def test_package_version_is_defined() -> None:
     assert __version__
+
+
+def test_data_package_exports_smd_container() -> None:
+    assert SMDMachineSplit.__name__ == "SMDMachineSplit"
